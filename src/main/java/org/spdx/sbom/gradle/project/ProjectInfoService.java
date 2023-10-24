495ecdae6c8b70de77f00890e0ee475345e3e767
Appu Goundan <appu@google.com>
2023-10-24 14:25:12 -0400

    Compute all project info once
    Signed-off-by: Appu Goundan <appu@google.com>

---
+++ src/main/java/org/spdx/sbom/gradle/project/ProjectInfoService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 The Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spdx.sbom.gradle.project;
+
+import java.util.Set;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.api.services.BuildService;
+import org.gradle.api.services.BuildServiceParameters;
+import org.spdx.sbom.gradle.project.ProjectInfoService.RootProjectParam;
+
+public abstract class ProjectInfoService implements BuildService<RootProjectParam> {
+
+  public interface RootProjectParam extends BuildServiceParameters {
+    SetProperty<ProjectInfo> getAllProjects();
+  }
+
+  private final Set<ProjectInfo> allProjects;
+
+  public ProjectInfoService() {
+    allProjects = getParameters().getAllProjects().get();
+  }
+
+  public Set<ProjectInfo> getAllProjectInfo() {
+    return allProjects;
+  }
+}
